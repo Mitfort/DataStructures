db7e90cb9534f986d776ea9ace940d307e3b8932
Mitfort <110680320+Mitfort@users.noreply.github.com>
2024-12-07 17:26:50 +0100

    Add files via upload

---
+++ kolejka.h
@@ -0,0 +1,82 @@
+#pragma once
+
+#ifndef MYSTRUCT_H
+#define MYSTRUCT_H
+
+#include<iostream>
+#include<string>
+
+template <typename T>
+struct Node{
+	T data;
+	Node* nast = nullptr;
+};
+
+template<typename T,typename... Args>
+void add(Node<T>*&kolejka, Args... args)
+{
+	T object{std::forward<Args>(args)...};
+
+	if (kolejka == nullptr)
+	{
+		Node<T>* nowy = new Node<T>{ object ,nullptr};
+		kolejka = nowy;
+		return;
+	}
+
+	Node<T>* w = kolejka;
+
+	while (w->nast != nullptr)
+	{
+		w = w->nast;
+	}
+
+	Node<T>* nowy = new Node<T>{ object, nullptr };
+	w->nast = nowy;
+}
+
+template<typename T>
+void remove(Node<T>*& kolejka)
+{
+	if (kolejka == nullptr)
+	{
+		std::cout << "Kolejka jest pusta";
+		return;
+	}
+
+	Node<T>* copy = kolejka;
+	kolejka = kolejka->nast;
+	delete copy;
+}
+
+template<typename T>
+int size(Node<T>* kolejka)
+{
+	int rozm = 0;
+
+	while (kolejka != nullptr)
+	{
+		kolejka = kolejka->nast;
+		rozm++;
+	}
+
+	return rozm;
+}
+
+template<typename T>
+void display(Node<T>* kolejka)
+{
+	if (kolejka == nullptr)
+	{
+		std::cout << "Kolejka jest pusta" << '\n';
+		return;
+	}
+
+	while (kolejka != nullptr)
+	{
+		std::cout << kolejka->data.imie << '\n';
+		kolejka = kolejka->nast;
+	}
+}
+
+#endif 
