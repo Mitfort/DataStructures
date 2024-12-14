d712f3c44ca34dc58057fdd054dc483c51a74c5b
Mateusz Poszelężny <110680320+Mitfort@users.noreply.github.com>
2024-12-14 16:32:04 +0100

    Update stos.h

--- stos.h
+++ stos.h
@@ -9,21 +9,27 @@ template<typename T>
 struct Node {
 	T object;
 	Node* poprz = nullptr;
+	void add(Node<T>*& stos, T value);
+	void remove(Node<T>*& stos);
+	void check_top(Node<T>* stos);
+	int rozmiar(Node<T>* stos);
+	bool czy_pusty(Node<T>* stos);
+	void wypisz(Node<T>* stos);
 };
 
 template<typename T>
-void add(Node<T>*& stos, T value)
+void Node<T>::add(Node<T>*& stos, T value)
 {
 	Node<T>* nowy = new Node<T>{ value,stos };
 	stos = nowy;
 }
 
 template<typename T>
-void remove(Node<T>*& stos)
+void Node<T>::remove(Node<T>*& stos)
 {
 	if (stos == nullptr)
 	{
-		cout << "Stos jest pusty";
+		std::cout << "Stos jest pusty";
 		return;
 	}
 
@@ -35,19 +41,19 @@ void remove(Node<T>*& stos)
 }
 
 template<typename T>
-void check_top(Node<T>* stos)
+void Node<T>::check_top(Node<T>* stos)
 {
 	if (stos == nullptr)
 	{
-		cout << "Stos jest pusty" << '\n';
+		std::cout << "Stos jest pusty" << '\n';
 		return;
 	}
 
-	cout << "Na wierzchu stosu: " << stos->object << '\n';
+	std::cout << "Na wierzchu stosu: " << stos->object << '\n';
 }
 
 template<typename T>
-int rozmiar(Node<T>* stos)
+int Node<T>::rozmiar(Node<T>* stos)
 {
 	int rozm = 0;
 
@@ -57,29 +63,29 @@ int rozmiar(Node<T>* stos)
 		stos = stos->poprz;
 	}
 
-	cout << "Rozmiar stosu: " << rozm << '\n';
+	std::cout << "Rozmiar stosu: " << rozm << '\n';
 	return rozm;
 }
 
 template<typename T>
-bool czy_pusty(Node<T>* stos)
+bool Node<T>::czy_pusty(Node<T>* stos)
 {
 	if (stos == nullptr)
 	{
-		cout << "Stos jest pusty" << '\n';
+		//std::cout << "Stos jest pusty" << '\n';
 		return true;
 	}
 
-	cout << "Stos nie jest pusty" << '\n';
+	//std::cout << "Stos nie jest pusty" << '\n';
 	return false;
 }
 
 template<typename T>
-void wypisz(Node<T>* stos)
+void Node<T>::wypisz(Node<T>* stos)
 {
 	if (stos == nullptr)
 	{
-		cout << "Stos jest pusty" << '\n';
+		std::cout << "Stos jest pusty" << '\n';
 		return;
 	}
 
@@ -88,11 +94,11 @@ void wypisz(Node<T>* stos)
 
 	while (stos != nullptr)
 	{
-		cout << "[";
+		std::cout << "[";
 		int* tab = new int[3];
 		for (int i = 0; i < 3; i++)
 		{
-			tab[i] = -1; 
+			tab[i] = -1;
 		}
 
 		int i = 2;
@@ -107,15 +113,15 @@ void wypisz(Node<T>* stos)
 		{
 			if (tab[i] != -1)
 			{
-				cout << tab[i];
+				std::cout << tab[i];
 			}
 			else
 			{
-				cout << " ";
+				std::cout << " ";
 			}
 		}
 
-		cout << "] " << stos->object << '\n';
+		std::cout << "] " << stos->object << '\n';
 		stos = stos->poprz;
 		idx = idxCopy + 1;
 		idxCopy++;
